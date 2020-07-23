890c33f5c45a6ea82c29e6d557b7a1069952e04f
127001david <127.0.0.1.david@gmail.com>
2020-07-23 16:14:28 +0800

    [feat]消息通知架构

---
+++ android/app/src/main/java/com/rightpoint/lite_chat/channel/ChannelConversation.java
@@ -0,0 +1,16 @@
+package com.rightpoint.lite_chat.channel;
+
+import com.rightpoint.lite_chat.MainActivity;
+
+/**
+ * Description：
+ * @author Wonder Wei
+ * Create date：2020/7/23 4:00 PM 
+ */
+public class ChannelConversation {
+    static final String CHANNEL_CONVERSATION = "com.rightpoint.litechat/conversation";
+
+    public static void connect(MainActivity activity) {
+
+    }
+}
